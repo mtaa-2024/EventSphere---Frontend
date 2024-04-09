36d00aea5e948f36d05b79e83eac1f976111c320
Matúš Políček <91195468+MatusPolicek@users.noreply.github.com>
2024-04-09 17:52:40 +0200

    colorScheme

--- app/src/main/java/stuba/fiit/sk/eventsphere/ui/theme/Color.kt
+++ app/src/main/java/stuba/fiit/sk/eventsphere/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package stuba.fiit.sk.eventsphere.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val green = Color(0xFF65B741)
+val white = Color(0xFFFFFFFF)
+val black = Color(0xFF252525)
+val lightGreen = Color(0xFF98DA82)
--- app/src/main/java/stuba/fiit/sk/eventsphere/ui/theme/Theme.kt
+++ app/src/main/java/stuba/fiit/sk/eventsphere/ui/theme/Theme.kt
@@ -16,31 +16,34 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    background = black,
+    surface = black,
+    onSurface = white,
+    onBackground = white,
+    primary = green,
+    onPrimary = white,
+    secondary = white,
+    onSecondary = black,
+    outline = green,
+    primaryContainer = lightGreen
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    background = white,
+    surface = white,
+    onSurface = black,
+    onBackground = black,
+    primary = green,
+    onPrimary = black,
+    secondary = black,
+    onSecondary = white,
+    outline = green,
+    primaryContainer = lightGreen
 )
 
 @Composable
 fun EventSphereTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
